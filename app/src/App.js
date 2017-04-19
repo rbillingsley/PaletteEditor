aab2186cfcd2ab5843308a6fde205dd9f93d79ed
Russell Billingsley <rbillingsley@users.noreply.github.com>
2017-04-19 15:43:06 +0100

    Added basic app skeleton from create-react-app

---
+++ app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <h1>Palette Editor</h1>
+        </div>
+        <p className="App-intro">
+          Welcome to the Palette Editor.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
